23160c40bf531dff2474c9ac61d17f3329afecb5
d-alio <darya_aliokhina@epam.com>
2026-08-26 22:35:53 +0400

    Steps added to Tests Tests divided to HomePageTests and CareerPageTests

--- TestLayer/Tests/HomePageTest.cs
+++ TestLayer/Tests/HomePageTest.cs
@@ -2,15 +2,13 @@
 using CoreLayer;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
 using SearchModel = TestLayer.Data.SearchModel;
 //Test comment
 
 namespace TestLayer.Tests
 {
     [TestFixture]
-    public class HomeTests : BaseTest
+    public class HomePageTests : BaseTest
     {
         private HomePage _homePage;
         private SearchResultPage _searchResultPage;
@@ -25,23 +23,38 @@ namespace TestLayer.Tests
             _careerPage = new CareerPage(WebDriverWrapper);
         }
 
-        [TestCase("Automation", "search?q=Automation")]
+        [TestCase("dotnet", "search?q=dotnet")]
         public void SearchOnIndexPageTest(string textToFind, string searchUrl)
         {
-            Logger.Information("Starting the test 'SearchOnIndexPageTest_Updated'.");
+            Logger.Information("Starting the test 'SearchOnIndexPageTest'.");
+
+            //Step 1: Accept cookies pop up
+            Logger.Information("Step 1 started: Accept cookies");
             _homePage.Cookies.AcceptCookies();
+            Logger.Information("Passed: Cookies accepted");
 
+            //Step 2: Click on Search icon
+            Logger.Information("Step 2 started: Click on Search icon");
             var searchPanel = _homePage.Header.ClickSearchIcon();
+            Logger.Information("Passed: Click on Search icon");
 
+            //Step 3: Enter keys received as test parameter and click on Find button
+            Logger.Information($"Step 3 started: Enter keys '{textToFind}' received as test parameter and click on Find button");
             searchPanel.EnterSearchTextUsingActions(textToFind)
                 .ClickFindButton();
+            Logger.Information($"Passed: Keys '{textToFind}' received as test parameter and Find button clicked");
 
+            //Step 4: Check that URL with results contains the entered keys parameter
+            Logger.Information($"Step 4 started: Check that URL with results contains the entered keys parameter '{textToFind}'");
             var currentUrl = WebDriverWrapper.GetUrl();
             Assert.That(currentUrl, Does.Contain(searchUrl));
+            Logger.Information($"Passed: URL with results contains the entered keys parameter '{textToFind}'");
 
-
+            //Get all the results elements
             var results = _searchResultPage.SearchResults.GetResults();
+            Logger.Information($"Search results have been received. {results.Count()} page card(s) is/are displayed on the page.");
 
+            //Step 5: Check that results contain the entered keys parameter
             bool allContainText = true;
             foreach (var result in results)
             {
@@ -53,75 +66,43 @@ namespace TestLayer.Tests
             }
             Assert.That(allContainText, Is.True, $"Not all search results contain '{textToFind}'");
 
-
             Logger.Information("Ending the test 'SearchOnIndexPageTest'.");
         }
 
 
-        [TestCase("Middle Automation Tester in Java")]
-        public void ValidateThatTheUserCanSearchForPositionBasedOnCriteria(string textToFind)
-        {
-            Logger.Information("Starting the test 'ValidateThatTheUserCanSearchForPositionBasedOnCriteria'.");
-            _homePage.Cookies.AcceptCookies();
-
-            _homePage.HeaderMenu.ClickMenuLink("Careers");
-
-            // click on 'Start your search' button
-            _careerPage.FindYourCareerButton.ClickFindCareerButton();
-
-            //accept cookies because user is redirected to sub-domen careers.epam.com
-            _homePage.Cookies.AcceptCookies();
-
-            //Select appropriate options for search
-            _careerPage.CareerSearch.DeselectLocations();
-            _careerPage.CareerSearch.SelectRemoteCareerOption();
-            _careerPage.CareerSearch.EnterCareerSearchTextUsingActions(textToFind);
-            _careerPage.CareerSearch.ClickFindCareerButton();
-
-
-            //Analyze search results
-            //Click on last career card
-            _searchResultPage.SearchResults.ClickOnParticularCareerResultTitle(_searchResultPage.SearchResults.GetResultsTitles().Count - 1);
-
-            var allElements = WebDriverWrapper.GetAllElements();
-            bool allContainText = true;
-            foreach (IWebElement element in allElements)
-            {
-                if (!element.Text.Contains(textToFind, StringComparison.OrdinalIgnoreCase))
-                {
-                    allContainText = false;
-                    break;
-                }
-            }
-            Assert.That(allContainText, Is.True, $"Not all elements contain '{textToFind}'");
-
-
-            Logger.Information("Ending the test 'SearchOnIndexPageTest'.");
-        }
-
-
-
-
-
-
-
+        
         [TestCaseSource(nameof(SearchModelData))]
         public void SearchOnIndexPageTest_JSON(SearchModel searchModelItems)
         {
             Logger.Information("Starting the test 'SearchOnIndexPageTest_JSON'.");
+
+            //Step 1: Accept cookies pop up
+            Logger.Information("Step 1 started: Accept cookies");
             _homePage.Cookies.AcceptCookies();
+            Logger.Information("Passed: Cookies accepted");
 
+            //Step 2: Click on Search icon
+            Logger.Information("Step 2 started: Click on Search icon");
             var searchPanel = _homePage.Header.ClickSearchIcon();
+            Logger.Information("Passed: Click on Search icon");
 
+            //Step 3: Enter keys received as test parameter and click on Find button
+            Logger.Information($"Step 3 started: Enter keys from the file '{searchModelItems.TextToSearch}' as test parameter and click on Find button");
             searchPanel.EnterSearchTextUsingActions(searchModelItems.TextToSearch)
                 .ClickFindButton();
+            Logger.Information($"Passed: Keys '{searchModelItems.TextToSearch}' received as test parameter and Find button clicked");
 
+            //Step 4: Check that URL with results contains the entered keys parameter
+            Logger.Information($"Step 4 started: Check that URL with results contains the entered keys parameter '{searchModelItems.TextToSearch}'");
             var currentUrl = WebDriverWrapper.GetUrl();
             Assert.That(currentUrl, Does.Contain(searchModelItems.SearchUrl));
+            Logger.Information($"Passed: URL with results contains the entered keys parameter '{searchModelItems.TextToSearch}'");
 
-
+            //Get all the results elements
             var results = _searchResultPage.SearchResults.GetResults();
+            Logger.Information($"Search results have been received. {results.Count()} page card(s) is/are displayed on the page.");
 
+            //Step 5: Check that results contain the entered keys parameter
             bool allContainText = true;
             foreach (var result in results)
             {
@@ -138,25 +119,21 @@ namespace TestLayer.Tests
         }
 
 
+        //[TestCaseSource(nameof(SearchModelData))]
+        //public void SearchOnIndexPageTest_Updated_WithJsonData(SearchModel searchModelItem)
+        //{
+        //    Logger.Information("Starting the test 'SearchOnIndexPageTest_Updated_WithJsonData'.");
 
+        //    var searchPanel = _homePage.Header.ClickSearchIcon();
 
+        //    searchPanel.EnterSearchTextUsingActions(searchModelItem.TextToSearch)
+        //        .ClickFindButton();
 
+        //    var currentUrl = WebDriverWrapper.GetUrl();
+        //    Assert.That(currentUrl, Does.Contain(searchModelItem.SearchUrl));
 
-        [TestCaseSource(nameof(SearchModelData))]
-        public void SearchOnIndexPageTest_Updated_WithJsonData(SearchModel searchModelItem)
-        {
-            Logger.Information("Starting the test 'SearchOnIndexPageTest_WithJson'.");
-
-            var searchPanel = _homePage.Header.ClickSearchIcon();
-
-            searchPanel.EnterSearchTextUsingActions(searchModelItem.TextToSearch)
-                .ClickFindButton();
-
-            var currentUrl = WebDriverWrapper.GetUrl();
-            Assert.That(currentUrl, Does.Contain(searchModelItem.SearchUrl));
-
-            Logger.Information("Ending the test 'SearchOnIndexPageTest_Updated'.");
-        }
+        //    Logger.Information("Ending the test 'SearchOnIndexPageTest_Updated_WithJsonData'.");
+        //}
 
         private static List<SearchModel> SearchModelData
         {
